5e34359f15d1fc10a0c508d6eb2c702e8ece9a26
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-15 16:50:29 -0400

    Initial Commit

---
+++ WatchOSNotes Watch App/WatchOSNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSNotesApp.swift
+//  WatchOSNotes Watch App
+//
+//  Created by Lori Rothermel on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSNotes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
